7e10ee473aee259b11b9acb56069e24c908c9aec
namiranda <naylenmiranda@gmail.com>
2019-05-05 21:58:29 -0300

    Update Parcial1.cpp
    modifico toPrint()

--- Parcial1.cpp
+++ Parcial1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 typedef string tipolista;
 
 class Nodo{
-    protected:
+    protected: 
         tipolista dato;
         Nodo *next;
     public:
@@ -33,7 +33,7 @@ class Lista{
             Lista *resto(void);
             Nodo *last(void);
             void borrarLast();
-            string toPrint(string p);
+            string toPrint();
 };
 
 class Pila:public Lista{
@@ -43,74 +43,56 @@ class Pila:public Lista{
              tipolista tope(void){return cabeza();};
              void desapilar(void){del();};
              bool pilavacia(){return esvacia();};
-};
+};                  
 
 //-----------------metodos de Lista-------------------------
 
-void Lista::del(void)
+void Lista::del(void) //borra la cabeza
 {    Nodo *aux;
      aux=czo;
      czo=czo->get_next();
      delete aux;
 }
 void Lista::add(tipolista d) //agrega un nodo al final de la lista
-{
+{  
 	Nodo *nuevo=new Nodo(d);
 	last()->set_next(nuevo);
-
+    
 }
 bool Lista::esvacia(void)
-{
+{   
     return czo->es_vacio();
 }
 
 tipolista Lista::cabeza(void)
-{
+{ 
   if(esvacia()){
                 cout<<" Error, Cabeza de lista vacia";
-                return "" ;
+                return "" ; 
   }
   return czo->get_dato();
 }
 
 Lista *Lista::resto(void)
-{
+{ 
       Lista *l=new Lista(czo->get_next());
       return (l);
 }
 
-// funcion precedencia
-// prcd(o1,'(') = 0 para todo o1 != de ')'
-// prcd(o1,')') = 1 para todo o1 != de '('
-// prcd('(',o2) = 0 para todo 02
-// prcd(o1,o2)  = 1 para todo o1 >= precedencia que o2
-int prcd(int o1,int o2)
-{ if(o2=='(')
-	 if(o1==')') return 1;
-            else return 0;
-  if(o2==')')
-	 if(o1=='(') return 0;
-		    else return 1;
-  if(o1=='(') return 0;
-
-  if((o1=='*')||(o1=='/')) return 1;
-  if(((o1=='+')||(o1=='-'))&&((o2=='+')||(o2=='-'))) return 1;
-						                        else return 0;
-}
-
-string Lista::toPrint(string p){
-     if (this->esvacia()) {
-        return p;
+string Lista::toPrint(){ 
+     string s= "";
+	 if (this->esvacia()) {
+        return s;
      } else {
-       std::ostringstream stm;
-       stm << this->cabeza()<<endl<< this->resto()->toPrint(p);
-
-       return stm.str();
+        s += "\n" + this->cabeza() + this->resto()->toPrint();
+       return s;
      }
 }
 Nodo* Lista::last(void){
-	if(czo->get_next()==NULL) return czo;
-	else resto()->last();
+		if(czo->get_next()==NULL) return czo;
+		else resto()->last();
+	
+	
 }
 
 //------------------Fin metodos de lista----------------------------
@@ -119,23 +101,25 @@ int main(){
 	fstream archivo;
 	string instruccion;
 	Lista *listaIns = new Lista(); //puntero a lista con instrucciones
-
+	
 	archivo.open("programa.txt", ios::in); //abre archivo en modo lectura
-
+	
 	if(archivo.fail()){
 		cout << "No se pudo abrir el archivo";
 		exit(1);
 	}
-
+	
 	while(!archivo.eof()){ // mientras no sea el final del archivo
 		getline(archivo, instruccion);
 		listaIns->add(instruccion);
 	}
-	//cout<<listaIns->cabeza();
-	cout<<listaIns->toPrint("fin");
-	cout<< listaIns->last()->get_dato();
+	cout<<"cabeza: " << listaIns->cabeza();
+	cout<<listaIns->toPrint();
+	cout<<"cabeza: " << listaIns->cabeza();
+	listaIns->del();
+	cout<<listaIns->toPrint();
 
-	archivo.close();
-
-
+	archivo.close();	
+		
+	
 }
